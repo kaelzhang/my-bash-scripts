68f926b31d54eed34cf5ba54b26be7429ddb92e7
Kael <i@kael.me>
2015-09-25 15:10:21 +0800

    first commit

---
+++ bash/git.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "load my git scripts"
+
+# Enhanced `git pull --rebase`
+#
+gl(){
+  current_branch=`_git_get_current_branch`
+  echo "git pull --rebase origin $current_branch"
+  git pull --rebase origin $current_branch
+}
+
+# Enhanced `git push`
+#
+gp(){
+  current_branch=`_git_get_current_branch`
+  echo "git push origin $current_branch $@"
+  git push origin $current_branch $@
+}
+
+glp(){
+  current_branch=`_git_get_current_branch`
+  echo "git pull --rebase origin $current_branch"
+  git pull --rebase origin $current_branch
+  if [[ $? -ne 0 ]] ; then
+     return $?
+  else
+    echo
+    echo "git push origin $current_branch $@"
+    git push origin $current_branch $@
+  fi
+}
+
+_git_get_current_branch(){
+  git symbolic-ref --short HEAD
+}
