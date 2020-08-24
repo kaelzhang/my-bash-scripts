dcab490a86285466ff7d4a5ab7ec8065008105ac
Kael Zhang <i+github@kael.me>
2020-08-24 20:40:01 +0800

    upgrade conda, and change env location of conda to user home

--- bash/conda.sh
+++ bash/conda.sh
@@ -9,14 +9,14 @@ conda-init() {
 
   # >>> conda initialize >>>
   # !! Contents within this block are managed by 'conda init' !!
-  local __conda_setup="$('/opt/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+  local __conda_setup="$($HOME/opt/anaconda3/bin/conda 'shell.bash' 'hook' 2> /dev/null)"
   if [ $? -eq 0 ]; then
     eval "$__conda_setup"
   else
-    if [ -f "/opt/anaconda3/etc/profile.d/conda.sh" ]; then
-      . "/opt/anaconda3/etc/profile.d/conda.sh"
+    if [ -f "~/opt/anaconda3/etc/profile.d/conda.sh" ]; then
+      . "~/opt/anaconda3/etc/profile.d/conda.sh"
     else
-      export PATH="/opt/anaconda3/bin:$PATH"
+      export PATH="~/opt/anaconda3/bin:$PATH"
     fi
   fi
   # <<< conda initialize <<<
