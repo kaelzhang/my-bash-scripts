d7fa31f3b3887cf7b4bfae83923f79cfc0cbb1b5
Kael Zhang <i+github@kael.me>
2024-01-11 11:09:22 +0800

    conda: upgrade init code from anaconda2 -> miniconda3

--- bash/conda.sh
+++ bash/conda.sh
@@ -4,21 +4,23 @@
 #  instead of auto initialization
 # ref: https://github.com/conda/conda/issues/8211
 conda-init() {
-  # Ported from Anaconda3 2020.08 installer
+  # Ported from Miniconda3 2020.08 installer
   #####################################################
 
   # >>> conda initialize >>>
   # !! Contents within this block are managed by 'conda init' !!
-  local __conda_setup="$($HOME/opt/anaconda3/bin/conda 'shell.bash' 'hook' 2> /dev/null)"
+  __conda_setup="$('/Users/kael/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
   if [ $? -eq 0 ]; then
     eval "$__conda_setup"
   else
-    if [ -f "~/opt/anaconda3/etc/profile.d/conda.sh" ]; then
-      . "~/opt/anaconda3/etc/profile.d/conda.sh"
+    if [ -f "/Users/kael/miniconda3/etc/profile.d/conda.sh" ]; then
+      . "/Users/kael/miniconda3/etc/profile.d/conda.sh"
     else
-      export PATH="~/opt/anaconda3/bin:$PATH"
+      export PATH="/Users/kael/miniconda3/bin:$PATH"
     fi
   fi
+
+  unset __conda_setup
   # <<< conda initialize <<<
 }
 
